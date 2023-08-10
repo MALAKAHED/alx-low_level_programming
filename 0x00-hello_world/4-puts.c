f815d92b61a3114436442411c41f37ec5b64f3f4
MALAKAHED <lokamalek776@gmail.com>
2023-08-10 13:53:43 -0700

    start learning c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-#include <stdio.h> int main () {     puts("Programming is like building a multilingual puzzle");     return (0); }
+#include <stdio.h>
+/*
+* main - The entry point
+*description : A C language program prints a sentence using the puts command
+*return 0
+*/
+int main(void)
+/* main - The entry point*/
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
