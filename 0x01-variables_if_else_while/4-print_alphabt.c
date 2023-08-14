c1deea567c2e48560c76d233e0cc2523298af51d
MALAKAHED <lokamalek776@gmail.com>
2023-08-14 13:06:07 -0700

    answers

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - main Entry point
+*description:A program that prints the English alphabet
+*Return: always 0
+*/
+int main(void)
+{
+char x;
+while (x <= 'z')
+{
+if (x != 'e' && x != 'q')
+{
+putchar(x);
+}
+x++;
+}
+putchar('\n');
+}
