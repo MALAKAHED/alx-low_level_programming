42143a196b3af800f1915b5658e2cbf45e276f8c
MALAKAHED <lokamalek776@gmail.com>
2023-08-14 10:58:58 -0700

    answers

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
-/**
-* main - main Entry point
-* description:A program that prints the English alphabet
-* Return: always 0
-*/
+/* main - main Entry point*/
+/* description:A program that prints the English alphabet*/
+/* Return: always 0 */
 int main(void)
 {
 char x;
