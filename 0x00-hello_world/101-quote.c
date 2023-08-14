a86e2ea18a65c5dafff726f0037a73fccbfb352a
MALAKAHED <lokamalek776@gmail.com>
2023-08-14 16:06:18 -0700

    adv in hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <Unistd.h>
 /**
-*main - A C program that prints a line to the standard error
-*Return 1(Success)
+*main - main Entry point
+*description:c program
+*Return: 1
 */
 int main(void)
 {
-fput("and that piece of art is useful\"-Dora Korpar, 2015-10-19\r", stdout);
-return (0);
+write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
 }
