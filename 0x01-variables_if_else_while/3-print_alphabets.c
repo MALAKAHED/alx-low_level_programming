f8947003985a15af15a747429078424b6e00ec31
MALAKAHED <lokamalek776@gmail.com>
2023-08-14 11:11:58 -0700

    answers && git push git add . && git commit -manswers

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-/* main - main Entry point*/
-/* description:A program that prints the English alphabet*/
-/* Return: always 0 */
+/**
+*main - main Entry point
+*description:A program that prints the English alphabet
+*Return: always 0
+*/
 int main(void)
 {
 char x;
