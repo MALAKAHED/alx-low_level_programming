1256de29ec1e1b51725c7012457af77cf55f5205
MALAKAHED <lokamalek776@gmail.com>
2023-08-21 07:35:58 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes a pointer.
+ * @n: is the int to be checked
+ * Return: always 0.
+ */
+void reset_to_98(int *n)
+{
+int n;
+*ptr = &n;
+*ptr = 98;
+return (0);
+}
