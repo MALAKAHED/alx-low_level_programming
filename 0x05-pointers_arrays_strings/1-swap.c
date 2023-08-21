5e91efbb12ffb1f2d34003b8baa822326fd8aee6
MALAKAHED <lokamalek776@gmail.com>
2023-08-21 10:18:41 -0700

    C - Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *  swap_int - a function that takes a pointer.
+ *  @a: is an int to be checked
+ *  @b: is an int to be checked
+ *  Return: always 0.
+ */
+void swap_int(int *a, int *b)
+{
+int a, b;
+a = 77;
+b = 88;
+printf("a=%d, b=%d\n", &b, &a);
+return (0);
+}
