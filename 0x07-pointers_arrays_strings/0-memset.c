98a8291dd15294ea55072ef1589fa742b606ce68
MALAKAHED <lokamalek776@gmail.com>
2023-08-28 11:10:37 -0700

    C - Even More functions, more nested loops

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,16 +3,16 @@
  * _memset - a function that fills memory with a constant byte.
  * @s: starting address of memory to be filled
  * @b: the desired value
- * @n: number of bytes to be changed 
- * Return: a pointer to the memory area s 
+ * @n: number of bytes to be changed
+ * Return: a pointer to the memory area s
 */
 char *_memset(char *s, char b, unsigned int n)
 {
-int a = 0; 
-for (; n > 0; a++) 
-{ 
-s[a] = b; 
-n--; 
+int a = 0;
+for (; n > 0; a++)
+{
+s[a] = b;
+n--;
 }
 return (s);
 }
