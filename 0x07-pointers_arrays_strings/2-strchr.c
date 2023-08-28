e6ceb67c800b91448278893739ead55d09963c8c
MALAKAHED <lokamalek776@gmail.com>
2023-08-28 13:58:51 -0700

    C - Even More functions, more nested loops

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,17 @@
 #include "main.h"
 /**
-* _strchr - a function that locates a character in a string.
-* @s: is a pointer
+* _strchr - Entry point
+* @s: input
 * @c: input
 * Return: Always 0 (Success)
 */
 char *_strchr(char *s, char c)
 {
-for (int i = 0; s[i] >= '\0'; i++)
+int i = 0;
+for (; s[i] >= '\0'; i++)
 {
 if (s[i] == c)
-{
 return (&s[i]);
-}
 }
 return (0);
 }
