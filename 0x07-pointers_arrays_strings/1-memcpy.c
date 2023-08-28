aae04b06a06cfef60afe6188d4f43965fcaf23b9
MALAKAHED <lokamalek776@gmail.com>
2023-08-28 13:42:41 -0700

    C - Even More functions, more nested loops

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,15 +6,12 @@
 *@n: number of bytes
 *Return: copied memory with n bytes changed
 */
-char *_memcpy(char *dest, char *src, unsigned int n)
-{
-destination memory area.
-int x = 0;
+int r = 0;
 int i = n;
-for (; x < i; x++)
+for (; r < i; r++)
 {
-dest[x] = src[x];
+dest[r] = src[r];
 n--;
 }
-return (dest);
+return (dest);
 }
