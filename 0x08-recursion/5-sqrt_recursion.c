537861c62eedc4b4a1f17bfc0c683090d963bab3
MALAKAHED <lokamalek776@gmail.com>
2023-08-30 15:58:56 -0700

    Shell, I/O Redirections and filters

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,7 @@
 #include "main.h"
+int actual_sqrt_recursion(int n, int i);
 /**
-* _sqrt_recursion - a function that returns the natural square root
+* _sqrt_recursion - returns the natural square root of a number
 * @n: number to calculate the square root of
 * Return: the resulting square root
 */
@@ -8,7 +9,10 @@ int _sqrt_recursion(int n)
 {
 if (n < 0)
 return (-1);
-return (actual_sqrt_recursion(n, 0));}
+return (actual_sqrt_recursion(n, 0));
+} 
+int actual_sqrt_recursion(int n, int i)
+{
 if (i * i > n)
 return (-1);
 if (i * i == n)
