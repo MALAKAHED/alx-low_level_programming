5f2b4fe5ea67a24e3435c3c3a23aaf80d5eee198
MALAKAHED <lokamalek776@gmail.com>
2023-08-29 19:14:00 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,14 +2,12 @@
 /**
  * add - adds two integers and returns the result
  * @a: int to be added to b
- * @b: int to b0e added to a
+ * @b: int to be added to a
  * Return: sum of a and b
  */
 int add(int a, int b)
 {
-int num1 = 5;
-int num2 = 10;
-int sum = add(num1, num2);
-printf("The sum of %d and %d is %d\n", num1, num2, sum);
-return 0;
+int c;
+c = a + b;
+return (c);
 }
