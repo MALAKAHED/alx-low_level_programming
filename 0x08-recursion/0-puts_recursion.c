57d182ec3b34a26e86dec66bd7c04f36cf327b15
MALAKAHED <lokamalek776@gmail.com>
2023-08-30 14:43:34 -0700

    Shell, I/O Redirections and filters

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,14 +2,14 @@
 /**
 * _puts_recursion - a function that prints a string, followed by a new line.
 * @s: input
-* Return: Return Always 0 (Success)
+* Return: Always 0 (Success)
 */
 void _puts_recursion(char *s)
 {
 if (*s)
 {
 _putchar(*s);
-_putchar_recursion(s + 1);
+_puts_recursion(s + 1);
 }
 else
 _putchr('\n');
