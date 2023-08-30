3925e4c77f4bfbb64dde6ae496981bcdd815af7b
MALAKAHED <lokamalek776@gmail.com>
2023-08-30 15:36:48 -0700

    Shell, I/O Redirections and filters

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,4 +9,9 @@ int _sqrt_recursion(int n)
 if (n < 0)
 return (-1);
 return (actual_sqrt_recursion(n, 0));}
+if (i * i > n)
+return (-1);
+if (i * i == n)
+return (i);
+return (actual_sqrt_recursion(n, i + 1));
 }
