e7f1c3eb83538fc51e5a4cc598c7c0ee79b2ee39
MALAKAHED <lokamalek776@gmail.com>
2023-08-30 16:19:53 -0700

    Shell, I/O Redirections and filters

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,9 +16,9 @@ return (actual_sqrt_recursion(n, 0));
  * square root of a number
  * @n: number to calculate the sqaure root of
  * @i: iterator
- *
- * Return : actual_sqrt_recursion
  */
+/* Return : actual_sqrt_recursion */
+
 int actual_sqrt_recursion(int n, int i)
 {
 if (i * i > n)
