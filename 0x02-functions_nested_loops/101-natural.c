54ee47ad5fefb12f4e0aa10db6c716670a19239d
MALAKAHED <lokamalek776@gmail.com>
2023-08-29 19:06:22 -0700

    C - Functions, nested loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,13 +1,9 @@
-/**
- * main - a program that computes and prints the sum of multiples
- * File: 101-natural.c
- * Auth: vincent kip
- */
 #include <stdio.h>
 /**
- * main - main Lists all the natural numbers below 1024 (excluded)
- * that are multiples of 3 or 5.
- * Return 0 
+ * main - Lists all the natural numbers below 1024 (excluded)
+ *        that are multiples of 3 or 5.
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
