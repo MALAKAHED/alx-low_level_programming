f532e6bfc63864df8a290ac95e380cac45df6be6
MALAKAHED <lokamalek776@gmail.com>
2023-09-13 10:34:29 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,11 +11,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 unsigned int x;
 if (array == NULL || action == NULL)
-{
 return;
-for (x = 0; i < size; x++)
+for (x = 0; x < size; x++)
 {
 action(array[x]);
-}
 }
 }
