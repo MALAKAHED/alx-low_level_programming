1fbd3c49d083ded1b4fd51ac545ad1b505719ee1
MALAKAHED <lokamalek776@gmail.com>
2023-09-16 01:21:45 -0700

    update

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,26 +1,22 @@
-#include "main.h"
-/**
-* wildcmp - Compare strings
-* @s1: pointer to string params
-* @s2: pointer to string params
-* Return: 0
-*/
-int wildcmp(char *s1, char *s2)
-{
-if (*s1 == '\0')
-{
-if (*s2 != '\0' && *s2 == '*')
-return (wildcmp(s1, s2 + 1));
+#include <stdio.h>
+int wildcmp(char *s1, char *s2);
+int main() {
+char str1[] = "hello";
+char str2[] = "h*llo";
+char str3[] = "h*ello";
+printf("%d\n", wildcmp(str1, str2));
+printf("%d\n", wildcmp(str1, str3));
+return 0;
 }
-return (*s2 == '\0');
+int wildcmp(char *s1, char *s2) {
+if (*s1 == '\0' && *s2 == '\0') {
+return 1;
 }
-if (*s2 == '*')
-{
-return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
+if (*s2 == '*') {
+return wildcmp(s1, s2 + 1) || (s1[0] != '\0' && wildcmp(s1 + 1, s2));
 }
-else if (*s1 == *s2)
-{
-return (wildcmp(s1 + 1, s2 + 1));
+if (*s1 == *s2) {
+return wildcmp(s1 + 1, s2 + 1);
 }
-return (0);
+return 0;
 }
