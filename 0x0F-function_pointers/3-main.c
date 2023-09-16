4d463332debf38f5b58a5eb00e404520cf5d5db2
MALAKAHED <lokamalek776@gmail.com>
2023-09-16 00:59:38 -0700

    update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,27 +10,27 @@
 */
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-int ele1, ele2;
+int num1, num2;
 char *op;
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-ele1 = atoi(argv[1]);
+num1 = atoi(argv[1]);
 op = argv[2];
-ele2 = atoi(argv[3]);
+num2 = atoi(argv[3]);
 if (get_op_func(op) == NULL || op[1] != '\0')
 {
 printf("Error\n");
 exit(99);
 }
-if ((*op == '/' && ele2 == 0) ||
-(*op == '%' && ele2 == 0))
+if ((*op == '/' && num2 == 0) ||
+(*op == '%' && num2 == 0))
 {
 printf("Error\n");
 exit(100);
 }
-printf("%d\n", get_op_func(op)(ele1, ele2));
+printf("%d\n", get_op_func(op)(num1, num2));
 return (0);
 }
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,56 +1,55 @@
 #include "3-calc.h"
-int el_add(int a, int b);
-int el_sub(int a, int b);
-int el_mul(int a, int b);
-int el_div(int a, int b);
-int el_mod(int a, int b);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
 /**
-* el_add - Returns the sum of two numbers.
+* op_add - Returns the sum of two numbers.
 * @a: The first number.
 * @b: The second number.
 * Return: The sum of a and b.
 */
-int el_add(int a, int b)
+int op_add(int a, int b)
 {
 return (a + b);
 }
 /**
-* el_sub - Returns the difference of two numbers.
+* op_sub - Returns the difference of two numbers.
 * @a: The first number.
 * @b: The second number.
 * Return: The difference of a and b.
 */
-int el_sub(int a, int b)
-{
-return (a - b);
+int op_sub(int a, int b)
+{	return (a - b);
 }
 /**
-* el_mul - Returns the product of two numbers.
+* op_mul - Returns the product of two numbers.
 * @a: The first number.
 * @b: The second number.
 * Return: The product of a and b.
 */
-int el_mul(int a, int b)
+int op_mul(int a, int b)
 {
 return (a * b);
 }
 /**
-* el_div - Returns the division of two numbers.
+* op_div - Returns the division of two numbers.
 * @a: The first number.
 * @b: The second number.
 * Return: The quotient of a and b.
 */
-int el_div(int a, int b)
+int op_div(int a, int b)
 {
 return (a / b);
 }
 /**
-* el_mod - Returns the remainder of the division of two numbers.
+* op_mod - Returns the remainder of the division of two numbers.
 * @a: The first number.
 * @b: The second number.
 * Return: The remainder of the division of a by b.
 */
-int el_mod(int a, int b)
+int op_mod(int a, int b)
 {
 return (a % b);
 }
