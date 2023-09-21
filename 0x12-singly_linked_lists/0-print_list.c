ac3349305a044956b21510702aa9e522d600a31d
MALAKAHED <lokamalek776@gmail.com>
2023-09-21 12:47:19 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,15 +7,15 @@
 */
 size_t print_list(const list_t *h)
 {
-size_t i = 0;
-while (h) // h = header
+size_t s = 0;
+while (h)
 {
 if (!h->str)
 printf("[0] (nil)\n");
 else
 printf("[%u] %s\n", h->len, h->str);
 h = h->next;
-i++;
+s++;
 }
-return (i);
+return (s);
 }
