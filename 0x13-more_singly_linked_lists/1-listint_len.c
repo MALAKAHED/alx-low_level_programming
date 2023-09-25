1d1d2caff0115cdaaedfdc4b9947c656d6d884eb
MALAKAHED <lokamalek776@gmail.com>
2023-09-25 10:22:30 -0700

    0x13-more singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+* listint_len - returns the number of elements
+* @h: linked list of type listint_t to traverse
+* Return: number of nodes
+*/
+size_t listint_len(const listint_t *h)
+{
+size_t counter = 0;
+while(h)
+{
+counter++;
+h->next;
+}
+return (counter);
+}
