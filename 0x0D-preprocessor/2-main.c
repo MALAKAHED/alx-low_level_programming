0df66fc60d87e6c63802c6481c9a5704c5a3ab26
MALAKAHED <lokamalek776@gmail.com>
2023-11-06 13:32:26 -0800

    0x0D. C - Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,8 +3,8 @@
  * main - a program that prints the name of the file
  * Return: always 0
  */
-int main (void)
+int main(void)
 {
-printf ("%s\n", __FILE__);
+printf("%s\n", __FILE__);
 return (0);
 }
