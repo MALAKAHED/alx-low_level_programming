8b30af21c3635c131a709defbd01ecb658c8d27d
MALAKAHED <lokamalek776@gmail.com>
2023-11-16 15:05:07 -0800

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * print_list - function that prints all the elements of a list_t list.
+ * @h: pointer for the head
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+	{
+		
+
+
+
+
+
+
+	}
---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+/**
+ * function that prints before the main function is executed.
+ */
+void after_mian()
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+main()
+{
+	printf("Inside main");
+}
