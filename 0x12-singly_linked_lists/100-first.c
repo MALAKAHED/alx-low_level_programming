83a9f0ff98064ea9a06588f96afaddf3e9afee33
MALAKAHED <lokamalek776@gmail.com>
2023-11-16 16:07:25 -0800

    0x12. C - Singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,11 @@
 #include "lists.h"
-/**
- * function that prints before the main function is executed.
- */
-void after_mian()
+void before_main(void)__attribute__ ((constructor));
+/*
+ * _before_main - prints a sentence before the main
+ * function is executed
+*/
+void _before_main(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
-main()
-{
-	printf("Inside main");
+printf("You're beat! and yet,you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
