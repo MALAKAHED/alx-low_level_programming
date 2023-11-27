25fc8952ae2c60e5b6ff444d892ad41569a35d47
MALAKAHED <lokamalek776@gmail.com>
2023-11-27 11:38:41 -0800

    pushing

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,9 +1,15 @@
-#ifndef LIST_H
-#define LIST_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+
+typedef struct list_s
+{
+    int data;
+    struct list_s *next;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
